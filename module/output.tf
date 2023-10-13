2675bd687dbcbc3a26e6695a271612e4aed2400d
machavarapu deepika <machavarapu.deepika@hcl.com>
2023-10-13 13:15:53 +0000

    uploading ansible-setup code

--- module/output.tf
+++ module/output.tf
@@ -5,7 +5,7 @@ output "controllerid" {
   value = google_compute_instance.controller.id
 }
 output "controller-selflink" {
-  value = google_compute_instance.controller.selflink
+  value = google_compute_instance.controller.self_link
 }
 output "host1-name" {
   value = google_compute_instance.host1.name
@@ -14,7 +14,7 @@ output "host1id" {
   value = google_compute_instance.host1.id
 }
 output "host1selflink" {
-  value = google_compute_instance.host1.selflink
+  value = google_compute_instance.host1.self_link
 }
 output "host2-name" {
   value = google_compute_instance.host2.name
@@ -23,5 +23,5 @@ output "host2id" {
   value = google_compute_instance.host2.id
 }
 output "host2-selflink" {
-  value = google_compute_instance.host2.selflink
+  value = google_compute_instance.host2.self_link
 }
